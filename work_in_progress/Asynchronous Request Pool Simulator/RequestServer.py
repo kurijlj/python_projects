fcee469b8a30037811f21f29f445040e4cca5d9d
Ljubomir Kurij <ljubomir_kurij@protonmail.com>
2024-01-02 14:57:42 +0100

    Implemented timer mechanism as a timer queue for tracking and reseting request pools of individual clients according to set RPM rate.

--- work_in_progress/Asynchronous Request Pool Simulator/RequestServer.py
+++ work_in_progress/Asynchronous Request Pool Simulator/RequestServer.py
@@ -66,14 +66,128 @@
 # =============================================================================
 # Modules import section
 # =============================================================================
-from fastapi import FastAPI
-from fastapi.responses import PlainTextResponse
-from threading import Thread
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import HTMLResponse
+from pydantic import BaseModel, Field
+from starlette import status
+from time import perf_counter, sleep
 
+
+# =============================================================================
+# User classes section
+# =============================================================================
+
+# -----------------------------------------------------------------------------
+# Class: RequestPool
+# -----------------------------------------------------------------------------
+#
+# Description:
+#   This class represents a request pool object that holds requests from a
+#   signle client IP address.
+#
+# Attributes:
+class RequestPool:
+    """RequestPool class."""
+    ip: str
+    tokens: int
+
+    def __init__(self, ip: str, tokens: int):
+        """Initialize the Request object."""
+        self.ip = ip
+        self.tokens = tokens
+    
+    def __str__(self):
+        """Return a string representation of the Request object."""
+        return f'Request(ip={self.ip}, tokens={self.tokens})'
+    
+    def reset_pool(self):
+        """Reset the request pool."""
+        self.tokens = 0
+
+# -----------------------------------------------------------------------------
+# Class: TimerQueue
+# -----------------------------------------------------------------------------
+#
+# Description:
+#   This class represents a timer queue object.
+#
+# Attributes:
+class TimerQueue:
+    """TimerQueue class."""
+    _queue: list
+
+    def __init__(self, time_limit: float):
+        """Initialize the TimerQueue object."""
+        self._queue = []
+        self._time_limit = time_limit
+    
+    def __iter__(self):
+        """Return the iterator for the TimerQueue object."""
+        return iter(self._queue)
+
+    def __str__(self):
+        """Return a string representation of the TimerQueue object."""
+        return f'TimerQueue(queue={self._queue})'
+
+    def add(self, request: RequestPool):
+        """Add a request to the queue."""
+        self._queue.append({'request': request, 'start': perf_counter()})
+
+    def check_elapsed(self):
+        """Reset the pool for the top most request pool if time limit is reached."""
+        if self._queue:
+            if perf_counter() - self._queue[0]['start'] > self._time_limit:
+                rp = self._queue.pop(0)['request']
+                rp.reset_pool()
+                self._queue.append({'request': rp, 'start': perf_counter()})
+
+
+# =============================================================================
+# User data models section
+# =============================================================================
+
+# -----------------------------------------------------------------------------
+# Class: Request
+
+class Request(BaseModel):
+    """Request data model."""
+    ip: str = Field(min_length=7, max_length=15)
+    tokens: int = Field(gt=1)
+
+    class Config:
+        json_schema_extra = {
+            'example': {
+                'ip': '192.168.0.1',
+                'tokens': 5
+            }
+        }
+
+
+# =============================================================================
+# User functions section
+# =============================================================================
+
+# -----------------------------------------------------------------------------
+# Function: pool_timers_loop
+# -----------------------------------------------------------------------------
+def pool_timers_loop():
+    """Check for elapsed timers."""
+    print('Timer queue loop started')
+    while True:
+        _pool.check_elapsed()
+        sleep(1)
 
 # =============================================================================
 # Global constants
 # =============================================================================
+TPM = 5
+
+
+# =============================================================================
+# Global variables
+# =============================================================================
+# _pool = []
+_pool = TimerQueue(60)
 
 
 # =============================================================================
@@ -83,27 +197,51 @@ from threading import Thread
 # Create FastAPI application object -------------------------------------------
 app = FastAPI()
 
-# Create a 'root' path operation decorator ------------------------------------
-@app.get('/')
+# Create the API entry for retrieving the list of requests --------------------
+# @app.get('/pool', status_code=status.HTTP_200_OK)
+@app.get('/pool', response_class=HTMLResponse)
+async def pool_status():
+    result = '<html><head><title>Request Pool Status</title></head><body>'
+    for item in _pool:
+        result += f'<p>{item["request"].ip}: {item["request"].tokens}</p>'
+    result += '</body></html>'
 
-# Define the path operation function ------------------------------------------
-def root():
-    """Return a greeting message."""
-    return {"message": "Hello World!"}
+    return result
+        
 
-# Define the fastapi app startup function -------------------------------------
-def startup():
-    import uvicorn
-    uvicorn.run(app, host='0.0.0.0', port=8000)
+# Create the API entry for adding requests to the pool ------------------------
+@app.post('/request', status_code=status.HTTP_201_CREATED)
+async def make_a_request(request: Request):
+    """Add a request to the pool."""
+    # Create a new request object
+    request_pool = RequestPool(**request.model_dump())
+
+    if _pool:
+        for item in _pool:
+            if item['request'].ip == request_pool.ip:
+                if item['request'].tokens + request_pool.tokens > TPM:
+                    raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
+                                        detail='RPM exceeded')
+                else:
+                    item['request'].tokens += request_pool.tokens
+                    return
+    
+    _pool.add(request_pool)
 
 
 # =============================================================================
 # Main program
 # =============================================================================
 if __name__ == '__main__':
-    # Start the web app in a separate thread ----------------------------------
-    fastapi_thread = Thread(target=startup)
-    fastapi_thread.start()
+    # Start the web app -------------------------------------------------------
+    import uvicorn
+
+    # Start the timer queue loop ----------------------------------------------
     
-    # Wait for the fastapi thread to finish -----------------------------------
-    fastapi_thread.join()
+    # Create a thread for the timer queue loop
+    import threading
+    timer_queue_thread = threading.Thread(target=pool_timers_loop, daemon=True)
+    timer_queue_thread.start()
+
+    # Run the web app ---------------------------------------------------------
+    uvicorn.run(app, host='0.0.0.0', port=8000)
